fd4943616df3c7eb168882000f2d18cd3f1515ac
Radithya Romero Al Giffary <radithya.romero@gmail.com>
2025-10-31 11:17:39 +0700

    fix config midtrans

--- src/config/midtrans.ts
+++ src/config/midtrans.ts
@@ -1,7 +1,14 @@
 import midtransClient from "midtrans-client";
 
-export const midtransSnap = new midtransClient.Snap({
-  isProduction: process.env.MIDTRANS_IS_PRODUCTION === "true",
-  serverKey: process.env.MIDTRANS_SERVER_KEY as string,
-  clientKey: process.env.MIDTRANS_CLIENT_KEY as string,
-});
+const useMidtransProduction = process.env.MIDTRANS_IS_PRODUCTION === "true";
+export const midtransSnap = useMidtransProduction
+  ? new midtransClient.Snap({
+      isProduction: true,
+      serverKey: process.env.MIDTRANS_SERVER_KEY as string,
+      clientKey: process.env.MIDTRANS_CLIENT_KEY as string,
+    })
+  : new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SANDBOX_SERVER_KEY as string,
+      clientKey: process.env.MIDTRANS_SANDBOX_CLIENT_KEY as string,
+    });
